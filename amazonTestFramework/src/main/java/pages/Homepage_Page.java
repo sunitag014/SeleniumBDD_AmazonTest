5a2c1365358c363be429c7cf8e49fc505c2b7d49
sunitag014 <sunita.v.gangurde@gmail.com>
2024-06-28 11:01:54 +0530

    updated page class

--- amazonTestFramework/src/main/java/pages/Homepage_Page.java
+++ amazonTestFramework/src/main/java/pages/Homepage_Page.java
@@ -11,82 +11,70 @@ import utilities.BaseClass;
 import java.time.Duration;
 
 public class Homepage_Page extends BaseClass {
-    public WebDriver driver;
-    public static String price,subTotalOnCartPage,subTotalOnProductPagePrice,priceOfProductOnCartPage;
+    private WebDriver driver;
+    public static String price, subTotalOnCartPage, subTotalOnProductPagePrice, priceOfProductOnCartPage;
     private final WebDriverWait wait;
+
     public Homepage_Page(WebDriver driver) {
         this.driver = driver;
         PageFactory.initElements(driver, this);
         wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-
     }
 
     public void navigateToURL(String url) {
-
-        BaseClass.driver.get(url);
-
+        driver.get(url);
     }
+
     public void searchInSearchBox(String searchText) throws InterruptedException {
-    WebElement searchBox =BaseClass.driver.findElement(By.id("twotabsearchtextbox"));
-    searchBox.clear();
-    searchBox.sendKeys(searchText);
-    searchBox.sendKeys(Keys.ENTER);
-Thread.sleep(3000);
+        WebElement searchBox = driver.findElement(By.id("twotabsearchtextbox"));
+        searchBox.clear();
+        searchBox.sendKeys(searchText);
+        searchBox.sendKeys(Keys.ENTER);
+        Thread.sleep(3000);
     }
-    public void verifyText(String expectedText)
-    {
-        String actualText = BaseClass.driver.findElement(By.xpath("//span[text()='Results']")).getText();
 
+    public void verifyText(String expectedText) {
+        String actualText = driver.findElement(By.xpath("//span[text()='Results']")).getText();
+        // Add logic to compare expectedText with actualText
     }
+
     public void clickOnFirstAddToCartButton(String index) throws InterruptedException {
         try {
             String addToCartXPath = "(//button[text()='Add to cart'])[" + index + "]";
-
-            WebElement addToCartButton =BaseClass.driver.findElement(By.xpath(addToCartXPath));
+            WebElement addToCartButton = driver.findElement(By.xpath(addToCartXPath));
             addToCartButton.click();
         } catch (Exception e) {
             e.printStackTrace();
             throw e; // Rethrow the exception to propagate it up the stack
         }
         Thread.sleep(3000);
-
-
     }
 
-    public void priceOfOnProductPage(String index)
-    {
-        price =null;
-        WebElement priceForFirstItem = BaseClass.driver.findElement(By.xpath("(//span[@class='a-price'])[" + index + "]"));
-        price= priceForFirstItem.getText();
-        price=price.concat(".00");
-        price=price.substring(1);
-        WebElement subTotalOnProductPage = BaseClass.driver.findElement(By.xpath("//span[@class='ewc-subtotal-amount']"));
-        subTotalOnProductPagePrice= subTotalOnProductPage.getText();
-        subTotalOnProductPagePrice=subTotalOnProductPagePrice.trim();
-        System.out.println("Price : " +price);
-        System.out.println("subTotalOnProductPagePrice:"+subTotalOnProductPagePrice);
-
+    public void priceOfOnProductPage(String index) {
+        price = null;
+        WebElement priceForFirstItem = driver.findElement(By.xpath("(//span[@class='a-price'])[" + index + "]"));
+        price = priceForFirstItem.getText();
+        price = price.concat(".00");
+        price = price.substring(1);
+        WebElement subTotalOnProductPage = driver.findElement(By.xpath("//span[@class='ewc-subtotal-amount']"));
+        subTotalOnProductPagePrice = subTotalOnProductPage.getText().trim();
+        System.out.println("Price : " + price);
+        System.out.println("subTotalOnProductPagePrice:" + subTotalOnProductPagePrice);
     }
-    public void clickOnCartButton()
-    {
-        WebElement cartButton = BaseClass.driver.findElement(By.id("nav-cart-count"));
+
+    public void clickOnCartButton() {
+        WebElement cartButton = driver.findElement(By.id("nav-cart-count"));
         cartButton.click();
-
     }
 
-    public void getPriceOfItem(String index){
-        WebElement priceOfSelectedItem = BaseClass.driver.findElement(By.xpath("(//span[@class='a-size-medium a-color-base sc-price sc-white-space-nowrap'])[" + index + "]"));
-        subTotalOnCartPage = priceOfSelectedItem.getText();
-        subTotalOnCartPage=subTotalOnCartPage.trim();
+    public void getPriceOfItem(String index) {
+        WebElement priceOfSelectedItem = driver.findElement(By.xpath("(//span[@class='a-size-medium a-color-base sc-price sc-white-space-nowrap'])[" + index + "]"));
+        subTotalOnCartPage = priceOfSelectedItem.getText().trim();
 
-        WebElement priceOfProduct = BaseClass.driver.findElement(By.xpath("//span[@class='a-size-medium a-color-base sc-price sc-white-space-nowrap sc-product-price a-text-bold']"));
-        priceOfProductOnCartPage= priceOfProduct.getText();
-        priceOfProductOnCartPage=priceOfProductOnCartPage.trim();
+        WebElement priceOfProduct = driver.findElement(By.xpath("//span[@class='a-size-medium a-color-base sc-price sc-white-space-nowrap sc-product-price a-text-bold']"));
+        priceOfProductOnCartPage = priceOfProduct.getText().trim();
 
-        System.out.println("subTotalOnCartPage: " +subTotalOnCartPage);
-        System.out.println("priceOfProductOnCartPage: " +priceOfProductOnCartPage);
-
+        System.out.println("subTotalOnCartPage: " + subTotalOnCartPage);
+        System.out.println("priceOfProductOnCartPage: " + priceOfProductOnCartPage);
     }
-
-
 }
